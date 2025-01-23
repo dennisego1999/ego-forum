570d6ace4e75f299a13efd976f024097190f9775
Dennis Ego <dennis.ego@artcoresociety.com>
2025-01-23 10:14:37 +0100

    Update Comment and Post factories

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -12,7 +14,10 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'commentable_id' => Post::factory(),
+            'commentable_type' => Post::class,
+            'body' => fake()->randomHtml,
         ];
     }
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -12,7 +13,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => fake()->title,
+            'body' => fake()->randomHtml,
         ];
     }
 }

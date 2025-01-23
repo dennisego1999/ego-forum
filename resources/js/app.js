f6ff639c070e615a9bb3b3b885f200f9281768d6
Dennis Ego <dennis.ego@artcoresociety.com>
2025-01-23 08:03:53 +0100

    Add sass

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
-import '../css/app.css';
+import '../scss/app.scss';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
@@ -9,15 +9,15 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+	title: (title) => `${title} - ${appName}`,
+	resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+	setup({ el, App, props, plugin }) {
+		return createApp({ render: () => h(App, props) })
+			.use(plugin)
+			.use(ZiggyVue)
+			.mount(el);
+	},
+	progress: {
+		color: '#4B5563'
+	}
 });
